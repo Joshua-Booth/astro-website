0b5f763bd4ef92d979e3fcdc53a259c2ffa90b50
Joshua Booth <me@joshuabooth.nz>
2021-09-01 23:27:59 +1200

    ✨ feat(base-template): add page meta tags

---
+++ src/constants/app.js
@@ -0,0 +1,16 @@
+/**
+ * Action types for reducers.
+ *
+ * @file app.js
+ * @module constants - App
+ * @author Joshua Booth
+ * @see https://github.com/joshua-booth/astro-website
+ */
+
+/**
+ * The name of the application.
+ *
+ * @constant
+ * @type {string}
+ */
+export const APP_NAME = "Joshua Booth";
