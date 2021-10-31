7ab7e05d84b3beb49f0edc8085ebd6fb397c894b
Joshua Booth <me@joshuabooth.nz>
2021-10-31 23:15:46 +1300

    ✨ feat(navigation): add scroll shadow effect

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,7 @@
 function Header({ title }) {
   return (
     <header className="fixed-top navigation w-full">
-      <nav className="bg-white shadow-sm px-3 py-2">
+      <nav id="navigation" className="px-3 py-2">
         <a href="/">
           <img className="logo w-10 h-10" src="/assets/logo.svg" alt={title} />
         </a>
